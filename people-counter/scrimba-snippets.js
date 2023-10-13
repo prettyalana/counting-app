d6122c78a1ac974fc3b60a5dd9d554d654dc2b3b
prettyalana <129349619+prettyalana@users.noreply.github.com>
2023-10-12 22:55:43 -0500

    Added files to the staging area

---
+++ people-counter/scrimba-snippets.js
@@ -0,0 +1,141 @@
+// document.getElementById("website-visitors").innerText = 5
+
+// let firstBatch = 5;
+// let secondBatch = 20;
+
+// let count = firstBatch + secondBatch;
+
+// let count = 5;
+
+// count = 3 + 5;
+
+
+// console.log(count);
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+
+// let myAge = 23;
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+
+// function increment() {
+//     console.log("The button was clicked");
+// }
+
+// function countdown() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+// }
+
+// countdown()
+
+
+
+// countdown()
+
+
+// function Alana() {
+//     console.log("Is so pretty")
+//     console.log("she loves the color pink")
+//     console.log("she also has long curly hair")
+//     console.log("she's a Fullstack developer, game developer, mom, wife, CEO, and businesswoman")
+//     console.log("is happy")
+//     console.log("Jag bor i Solna")
+
+// }
+
+// Alana()
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// let lapTotal = lap1 + lap2 + lap3;
+// console.log(lapTotal)
+
+// let lap1 = 34;
+//     let lap2 = 33;
+//     let lap3 = 36;
+
+// function laps() {
+
+// let lapTotal = lap1 + lap2 + lap3;
+// console.log(lapTotal)
+// }
+
+// laps()
+
+// let lapsCompleted = 0;
+
+// function laps() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// laps()
+// laps()
+// laps()
+
+// console.log(lapsCompleted)
+
+// document.getElementById("website-visitors").innerText = 5
+
+
+//    function Subscribe() {
+//       let name = prompt("What is your name?")
+//       prompt("What is your email?")
+//       alert ("Thanks we'll be in touch!")
+//    }
+//    let button = document.querySelector(button); button.addEventListener("Click", subscribe);
+
+// let websiteVisitors = document.getElementById("count")
+
+// console.log(websiteVisitors)
+// let count = 0;
+// if (localStorage.getItem ("count") === null) {
+//     localStorage.setItem ("count", count) 
+// } else {
+//     count = localStorage.getItem ("count")
+//     count = parseInt (count)
+// }
+// websiteVisitors.innerText = count
+
+// function increment() {
+//     count = count + 1
+//     websiteVisitors.innerText = count
+//     console.log(count)
+//     localStorage.setItem ("count", count)
+// }
+
+let username = "prettyalana"
+
+// console.log(username)
+
+let message = "You have three new notifications"
+
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
+
+let name = "Alana" 
+let greeting = "Hi, my name is "
+
+myGreeting = greeting + name
+
+console.log(myGreeting)
